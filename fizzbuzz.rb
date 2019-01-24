0138d4846fcfb2924c53319eb34632314239df91
Karen Vo <karenvo@usc.edu>
2019-01-24 13:00:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
+  end
+  if int % 5 == 0 
+    "Buzz"
+  if int % 15 == 0
+    "FizzBuzz"
+  end
+  if int % 4 == 0
+    "nil"
   end
 end
