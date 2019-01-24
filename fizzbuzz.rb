027860b1e1aea7d998d4dd73dad647a2087ab8f9
Karen Vo <karenvo@usc.edu>
2019-01-24 13:18:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif int % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif !(int % 3 || 5 == 0)
-    puts "nil"
+    "nil"
   end
 end
